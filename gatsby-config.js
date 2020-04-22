47b4ee98d36e168ebcc3c72287764a47a5ec9172
Dino Hasanic <dinohasanic@Dinos-MacBook-Pro.local>
2020-04-22 11:55:14 -0700

    Update page icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,41 @@
 module.exports = {
   siteMetadata: {
-    title: `Dino Hasanic | Full-Stack Developer`,
-    description: `Hi, I'm Dino, a full-stack developer based in the SF Bay Area. `,
-    author: `@dinohasanic`,
+    title: 'Dino Hasanic | Full-Stack Developer',
+    description: 'Hi, I\'m Dino, a full-stack developer based in the SF Bay Area. ',
+    author: '@dinohasanic',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `static`,
+        name: 'static',
         path: `${__dirname}/src/static`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/dinohasanic-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
